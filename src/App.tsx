3484144d23980240815133d4718d73850e3989a3
swimmingkiim <swimmingkiim@gmail.com>
2020-09-13 23:23:12 +0900

    delete files that I don't need and init typescript

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./sampleStyle";
+
+const App: React.FC = () => {
+  return <S.SampleDiv>hello world! this is sample text</S.SampleDiv>;
+};
+
+export default App;
---
+++ src/sampleStyle.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const SampleDiv = styled.div`
+  color: pink;
+`;
