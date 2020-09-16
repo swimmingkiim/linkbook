ab00df95d8659b5a362a593daa04f831a05b845c
swimmingkiim <swimmingkiim@gmail.com>
2020-09-16 22:51:39 +0900

    Updates

---
+++ precache-manifest.dee08616e0427bb694fbc8b1ddf1253d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5915ea96d7fb4a999516193b56f0ab",
+    "url": "/linkbook/index.html"
+  },
+  {
+    "revision": "b7a15f0a737108d4816b",
+    "url": "/linkbook/static/js/2.509426c4.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/linkbook/static/js/2.509426c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "091eb7c24b3fe801ab14",
+    "url": "/linkbook/static/js/main.9da7aab0.chunk.js"
+  },
+  {
+    "revision": "292ec2c5b27487e91a63",
+    "url": "/linkbook/static/js/runtime-main.551ce8f2.js"
+  }
+]);
